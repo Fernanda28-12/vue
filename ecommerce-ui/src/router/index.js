13db23ac317f92664250f3cae07725499d6f4e1b
Mafesaurus28 <127638957+Fernanda28-12@users.noreply.github.com>
2023-10-24 18:47:02 -0500

    Add files via upload

---
+++ ecommerce-ui/src/router/index.js
@@ -0,0 +1,43 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import AddCategory from "@/views/Category/AddCategory.vue";
+import Category from "@/views/Category/Category.vue";
+import EditCategory from "@/views/Category/EditCategory.vue";
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: "/admin/category/add",
+    name: "AddCategory",
+    component: AddCategory,
+  },
+  {
+    path: "/admin/category",
+    name: "AdminCategory",
+    component: Category,
+  },
+  {
+    path: "/admin/category/:id",
+    name: "EditCategory",
+    component: EditCategory,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
